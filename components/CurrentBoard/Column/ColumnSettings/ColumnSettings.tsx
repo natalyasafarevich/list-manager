ef01ccef1165c8d6a053db06c1aeb1ac0c393741
Natalya <natalyasafarevich@gmail.com>
2024-03-15 14:08:32 +0100

    navigation on colunm setting is setted

--- components/CurrentBoard/Column/ColumnSettings/ColumnSettings.tsx
+++ components/CurrentBoard/Column/ColumnSettings/ColumnSettings.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {getIsOpenClSetting, isCreateCard} from '@/store/column-setting/actions';
 import {AppDispatch, RootState} from '@/store/store';
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 type SettingsProps = {
@@ -10,30 +10,49 @@ type SettingsProps = {
 };
 
 const ColumnSettings: FC<SettingsProps> = ({setIsOpen, addNewCard}) => {
+  const [isCopy, setIsCopy] = useState(false);
+  const [title, setTitle] = useState('Действия со списком');
   const isOpen = useSelector((state: RootState) => state.cl_setting);
 
-  const dispatch: AppDispatch = useDispatch();
-
-  // const addNewCard = () => {
-  //   console.log(isOpen);
-  //   dispatch(isCreateCard({isCreate: true}));
-  //   dispatch(getIsOpenClSetting({isOpen: false}));
-  // };
-  // isCreateCard
-  // console.log(isOpen);
   return (
     <div className='position-absolute z-3 rounded-3 bg-warning-subtle card w-100  top-0 start-100 p-2'>
       <div className='d-flex justify-content-between align-items-center mb-1'>
-        <span>Действия со списком</span>
+        {isCopy && (
+          <button
+            onClick={() => {
+              setIsCopy(!isCopy);
+            }}
+          >
+            back
+          </button>
+        )}
+        <span>{title}</span>
         <button type='button' onClick={setIsOpen}>
           x
         </button>
+
         {/* <b>{name}</b> */}
         {/* <button className='btn btn-dark'>...</button> */}
       </div>
-      <div className=''>
-        <button onClick={addNewCard}>Добавить карточку</button>
-      </div>
+
+      {!isCopy && (
+        <>
+          <div className=''>
+            <button onClick={addNewCard}>Добавить карточку</button>
+          </div>
+          <div className=''>
+            <br />
+            <button
+              onClick={() => {
+                setIsCopy(!isCopy);
+              }}
+            >
+              копирование списка
+            </button>
+          </div>
+        </>
+      )}
+      {isCopy && <div>{/* <span>Копирование списка</span> */}</div>}
     </div>
   );
 };
