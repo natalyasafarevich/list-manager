1eb562ecd2d685cf45409f1d022d33e427ffda68
Natalya <natalyasafarevich@gmail.com>
2024-05-08 16:53:28 +0200

    profile page has been started

---
+++ app/profile/[uid]/page.tsx
@@ -0,0 +1,9 @@
+import UserProfileComponent from '@/components/UserProfileComponent/UserProfileComponent';
+
+export default function UserProfile() {
+  return (
+    <>
+      <UserProfileComponent />
+    </>
+  );
+}
---
+++ components/UserProfileComponent/UserProfileComponent.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react';
+
+const UserProfileComponent: FC = () => {
+  return (
+    <div className='user-profile'>
+      <div className='user-profile__container'>
+        <div className='user-profile__img'></div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileComponent;
