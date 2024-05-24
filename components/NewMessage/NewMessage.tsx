5e779daa84c80a4edeac58da33ef5c7fea972e26
Natalya <natalyasafarevich@gmail.com>
2024-05-24 11:23:41 +0200

    inbox has  been fixed

--- components/NewMessage/NewMessage.tsx
+++ components/NewMessage/NewMessage.tsx
@@ -1,24 +1,49 @@
 'use client';
 import {FC, useEffect, useState} from 'react';
-import ReactQuill, {Quill} from 'react-quill';
 import {updateUserData} from '@/helper/updateUserData';
-import ImageResize from 'quill-image-resize-module-react';
 import {useSelector} from 'react-redux';
 import {RootState} from '@/store/store';
 import {v4 as createId} from 'uuid';
-import {formats, modules} from '@/variables/edit';
 import PopupMessage from '../PopupMessage/PopupMessage';
 import 'react-quill/dist/quill.snow.css';
 import './NewMessage.scss';
+import dynamic from 'next/dynamic';
 
-Quill.register('modules/imageResize', ImageResize);
+const modules = {
+  toolbar: [
+    [{header: '1'}, {header: '2'}, {font: []}],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{list: 'ordered'}, {list: 'bullet'}, {indent: '-1'}, {indent: '+1'}],
+    ['link', 'image'],
+    ['clean'],
+  ],
+  clipboard: {
+    matchVisual: false,
+  },
+};
 
+const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+];
 interface NewMessageProps {
   setIsIOpen: (v: boolean) => void;
   recipientId?: string;
   recipientName?: string;
 }
-
+const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
 const NewMessage: FC<NewMessageProps> = ({recipientId, setIsIOpen, recipientName}) => {
   const [messageValue, setMessageValue] = useState('');
   const [subtitle, setSubtitle] = useState('');
