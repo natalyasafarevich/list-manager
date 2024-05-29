c9e1656453b1a3a5e1d7699cfcfea7af2c27de1d
Natalya <natalyasafarevich@gmail.com>
2024-05-29 17:03:18 +0200

    board has been fixed

--- components/CurrentBoard/Members/Members.tsx
+++ components/CurrentBoard/Members/Members.tsx
@@ -4,7 +4,6 @@ import 'firebase/auth';
 import AddMember from './AddMember/AddMember';
 import './Members.scss';
 import MiniPopup from '@/components/MiniPopup/MiniPopup';
-import ClickAwayListener from '@/components/ClickAwayListener/ClickAwayListener';
 
 const Members: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,13 +12,11 @@ const Members: FC = () => {
       <div className='new-members__container'>
         <button className='new-members__button' onClick={(e) => setIsOpen(!isOpen)}></button>
         {isOpen && (
-          // <ClickAwayListener setIsOpen={(e) => setIsOpen(e)}>
           <div className='new-members__popup'>
             <MiniPopup title='Add new members' setIsOpen={setIsOpen}>
               <AddMember setIsOpen={(e) => setIsOpen(e)} />
             </MiniPopup>
           </div>
-          // </ClickAwayListener>
         )}
       </div>
     </div>
