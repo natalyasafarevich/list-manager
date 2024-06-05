1f61f90c6b8fc4b8756e38a4979e484c2faa9f6f
Natalya <natalyasafarevich@gmail.com>
2024-06-05 14:45:40 +0200

    favicon is added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export const metadata: Metadata = {
     template: 'HiveMind',
     default: 'HiveMind',
   },
+  icons: {
+    icon: 'fdfgdf.ico',
+  },
   description: `HiveMind is a task manager that helps you organize, plan, and track your work with ease.
   Never lose important details and always stay on track with HiveMind's intelligent task management features.`,
 };
