73d4078708077b06ce711cab29eb126c415cc7d9
Natalya <natalyasafarevich@gmail.com>
2024-06-07 12:57:24 +0200

    metadata has been updated

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,5 +1,10 @@
 import Inbox from '@/components/Inbox/Inbox';
-
+import {Metadata} from 'next';
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Inbox',
+  },
+};
 export default function Page() {
   return <Inbox />;
 }
